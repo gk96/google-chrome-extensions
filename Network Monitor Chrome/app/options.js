a81df09c1b6763a22c0113b9025da63497e6fd36
Gautham K <gauthamk1111@gmail.com>
2020-04-26 21:43:57 +0530

    Speed Unit Changing Option Added

--- Network Monitor Chrome/app/options.js
+++ Network Monitor Chrome/app/options.js
@@ -7,6 +7,15 @@ function save_options(val) {
             console.log("option saved!!");
           });
   }
+  function save_speeds(val) {
+        
+    chrome.storage.sync.set({
+        "speed": val,
+      }, function() {
+        // Update status to let user know options were saved.
+        console.log("option saved!!");
+      });
+}
   
   // Restores select box and checkbox state using the preferences
   // stored in chrome.storage.
@@ -19,6 +28,13 @@ function save_options(val) {
                 e.checked = true;
       });
     });
+    chrome.storage.sync.get("speed", function (obj) {      
+        document.getElementsByName('speedChoice').forEach(e => {
+              
+              if (e.value == obj.speed)  
+                  e.checked = true;
+        });
+      });
   }
 
 document.addEventListener('DOMContentLoaded', function(){
@@ -38,6 +54,11 @@ document.addEventListener('DOMContentLoaded', function(){
                 });
             }
             save_options(choice.value)
+            });
+    });
+    document.getElementsByName('speedChoice').forEach(choice => {
+        choice.addEventListener('change',function(){
+            save_speeds(choice.value);
             });
     });
 });
