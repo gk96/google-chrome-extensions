e0e3a3d33a6eeaee3501306e211ae823837e06e7
Gautham K <gauthamk1111@gmail.com>
2020-05-01 21:43:11 +0530

    fixing install files for windows

--- Network Monitor Chrome/Network_Monitor-Windows/configure_windows.bat
+++ Network Monitor Chrome/Network_Monitor-Windows/configure_windows.bat
@@ -1,5 +1,4 @@
-:: Python Modules Installation
-
+:: Python3 Modules Installation
 pip install psutil
 
 
@@ -8,15 +7,24 @@ pip install psutil
 
 :: Moving All the files necessary
 mkdir %USERPROFILE%\AppData\Local\Programs\NetworkMonitorNative
+
+:: Copy Manifest Files
 copy com.gk.netmon.json %USERPROFILE%\AppData\Local\Programs\NetworkMonitorNative
 copy com.gk.netstat.json %USERPROFILE%\AppData\Local\Programs\NetworkMonitorNative
+
+:: Copy Python Files
 copy "%~dp0network_monitor.py" %USERPROFILE%\AppData\Local\Programs\NetworkMonitorNative
 copy "%~dp0netstat.py" %USERPROFILE%\AppData\Local\Programs\NetworkMonitorNative
+
+:: Copy Registry Installer and Uninstaller
 copy "%~dp0install_host.bat" %USERPROFILE%\AppData\Local\Programs\NetworkMonitorNative 
 copy "%~dp0uninstall_host.bat" %USERPROFILE%\AppData\Local\Programs\NetworkMonitorNative 
-copy "%~dp0native-messaging-example-host.bat" %USERPROFILE%\AppData\Local\Programs\NetworkMonitorNative 
 
+:: Copy Feature Files
+copy "%~dp0start_network_monitor.bat" %USERPROFILE%\AppData\Local\Programs\NetworkMonitorNative 
+copy "%~dp0start_netstat.bat" %USERPROFILE%\AppData\Local\Programs\NetworkMonitorNative
 
+:: Adding to Registry
 cd %USERPROFILE%\AppData\Local\Programs\NetworkMonitorNative
 install_host.bat
 
